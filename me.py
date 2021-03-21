d9103e742290fd3490c35a3887fe52f3d0fd29b7
Yassine Bouagaz <yassine@MacBook-Pro-van-Yassine.local>
2021-03-21 21:37:16 +0100

    groupBy

--- me.py
+++ me.py
@@ -1,26 +1,54 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
 
 def get_data():
     return pd.read_csv('./archive/unemployment.csv')
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 df = get_data()
 min_year = int(df['Year'].min()) 
 max_year = int(df['Year'].max()) 
 district_names = df['District Name'].unique()
+neighborhood_names = df['Neighborhood Name'].unique()
 
 st.title('Barcelona')
-st.table(df.head())
+
+#st.table(df.head())
 
 st.sidebar.header('Filter Options')
 
 selected_year = st.sidebar.slider('Year', min_year, max_year)
-##df[df['Year'] == selected_year]
 
-##selected_year = st.sidebar.selectbox('Year', list(reversed(range(2013,2018))))
 selected_district = st.sidebar.selectbox('District Name', district_names)
-df[(df['District Name'] == selected_district) & (df['Year'] == selected_year)]
 
+#selected_neighborhood = st.sidebar.selectbox('Neighborhood Name', neighborhood_names)
+df1 = df[(df['District Name'] == selected_district) 
+        & (df['Year'] == selected_year)]
+       # & (df['Neighborhood Name'] == selected_neighborhood)]
 
+df1
+
+#number per Neighborhood
+st.write("Number per Neighborhood")
+d1 = df1.groupby(df['Neighborhood Name'])['Number'].sum()
+d1
+
+#number per gender
+st.write("Number per Gender")
+d2 = df1.groupby(df['Gender'])['Number'].sum()
+d2
+
+#number per month
+st.write("Number per Month")
+d3 = df1.groupby(df['Month'])['Number'].sum()
+d3
+
+d3.hist()
+plt.show()
+st.pyplot()
+
+st.bar_chart(d3['Month'])
